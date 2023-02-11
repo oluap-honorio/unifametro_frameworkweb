e92e51fe79eb7ec101e884ea0c166e650c1d3611
biophas <biophas@gmail.com>
2023-02-11 15:40:17 -0300

    Finalizando Atividade 3

--- agrovinos/urls.py
+++ agrovinos/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+from ovino import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ovino/', include('ovino.urls')),
+    path('', views.index),
+]
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
