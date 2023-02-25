7b5db0f55d3d9e94966d73dfe0ca25b3291e965c
Rafael Laurindo <rafaellaurindomf@icloud.com>
2023-02-25 15:34:22 -0300

    chore: add template

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sekizai',
+    'widget_tweaks',
     'ovino.apps.OvinoConfig',
 ]
 
