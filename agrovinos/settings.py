4a86d79091340f1a522fd4cd02df0bea54d4cbf8
Bruno Moura <antoniobrunobm@gmail.com>
2023-02-25 16:13:00 -0300

    Finalizando atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ovino.apps.OvinoConfig',
     'sekizai',
-    'widget_tweaks'
+    'widget_tweaks',
+    'tag.apps.TagConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,7 +76,7 @@ TEMPLATES = [
             ],
         },
     },
-]
+]   
 
 LOGIN_REDIRECT_URL = '/ovino/'
 LOGOUT_REDIRECT_URL = '/'
@@ -87,9 +88,13 @@ WSGI_APPLICATION = 'agrovinos.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
+        "USER": os.environ.get("SQL_USER", "admin"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "1234"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
@@ -116,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
--- dockerfile
+++ dockerfile
@@ -13,6 +13,8 @@ ENV PYTHONUNBUFFERED 1
 RUN pip install --upgrade pip 
 COPY ./requirements.txt . 
 RUN pip install -r requirements.txt 
+RUN apk update \
+ && apk add postgresql-dev gcc python3-dev musl-dev
 
 # copy project
 COPY . .
