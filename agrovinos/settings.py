d50f58e704ca562d2fa243fade288c35c7b5ea1a
leandro.fernandes <leandro.fernandes@s4e.com.br>
2023-02-25 11:35:57 -0300

    Finalizando Atividade 4

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -19,6 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# Aviable: os.path.join(BASE_DIR, ...)
+import os
+
+...
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1pm*crjz8*(dzocwtdd+e76ushzy(uku2pm12x#+&m*%^)d6ar'
 
