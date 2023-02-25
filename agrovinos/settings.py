7f3daeca379e4e82aff72d60e43f79c2de858cf5
Vinicius <viniciusmr1499@gmail.com>
2023-02-25 14:05:48 -0300

    Finalizando Atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -20,6 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'agrovinos/static'),
+)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1pm*crjz8*(dzocwtdd+e76ushzy(uku2pm12x#+&m*%^)d6ar'
 
