a21b9934b35c2cb24909727bcf6a23753a8b6360
Monique Parente Lima <moniqueparentelima@gmail.com>
2023-02-25 14:51:21 -0300

    Atividade 5 concluida

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -138,4 +138,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'agrovinos/static'),
+)
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
