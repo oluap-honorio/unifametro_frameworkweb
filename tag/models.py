88b59543750f8a59e8d0da974d3b328f41d7e7e0
biophas <biophas@gmail.com>
2023-02-25 16:14:05 -0300

    Finalizando Atividade 5

---
+++ tag/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+
+
+class Tag(models.Model):
+    
+    serial = models.CharField(max_length=16, unique=True)
+    id = models.UUIDField(    primary_key=True,default=uuid.uuid4,null=False,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.serial
+# Create your models here.
