45665430ed63a74a8c418e9f7eff72402f93e0c3
Bruno Moura <antoniobrunobm@gmail.com>
2023-02-25 16:49:24 -0300

    Finalizando atividade 7

--- tag/models.py
+++ tag/models.py
@@ -8,6 +8,7 @@ class Tag(models.Model):
     id = models.UUIDField(    primary_key=True,default=uuid.uuid4,null=False,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(    primary_key=True,default=uuid.uuid4,null=False,blank=True)
 
     def __str__(self):
         return self.serial
