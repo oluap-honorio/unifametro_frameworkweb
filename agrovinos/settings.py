fe8329d9b644369194b08d8a6056619b68c2ad77
biophas <biophas@gmail.com>
2023-02-25 15:00:57 -0300

    Finalizando Atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -34,6 +34,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default=SECRET_KEY)
 if os.environ.get("DJANGO_ALLOWED_HOSTS"):
     ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -128,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'agrovinos/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
