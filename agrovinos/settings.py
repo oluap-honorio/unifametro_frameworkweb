dc823e4215d3782c1e1c583bb35da72226877d17
Denis Simplicio <simmplicidade@gmail.com>
2023-02-25 14:30:28 -0300

    Finalizando Atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -132,3 +132,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'agrovinos/static'),
+)
