5db3ac5c883c564f6645aed82876dbbe6f550402
Jefferson Vasconcelos <jefferson.vasconcelos29@gmail.com>
2023-02-25 15:46:52 -0300

    Finalizando Atividade 6

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ovino.apps.OvinoConfig',
+    'tag.apps.TagConfig',
     'sekizai',
     'widget_tweaks',
 
@@ -88,11 +89,16 @@ WSGI_APPLICATION = 'agrovinos.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
+        "USER": os.environ.get("SQL_USER", "admin"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "1234"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
+
 
 
 # Password validation
@@ -117,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
--- ovino/models.py
+++ ovino/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from tag.models import Tag
 
 class Ovino(models.Model):
     
+    tag = models.OneToOneField(Tag, on_delete=models.SET_NULL, null=True)
     identificador = models.CharField(max_length=16, unique=True)
     peso = models.FloatField()
     nascimento = models.DateTimeField()
