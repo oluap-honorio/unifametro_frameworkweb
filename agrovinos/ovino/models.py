f38d0db879703384b867540e5e0b77b7d8ed84e5
joao.lima07 <lab03@ACADEMICO.LOCAL>
2024-01-27 10:59:24 -0600

    iniciando ativ 2

---
+++ agrovinos/ovino/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Ovino(models.Model):
+    
+    identificador = models.CharField(max_length=16, unique=True)
+    peso = models.FloatField()
+    nascimento = models.DateTimeField()
+    raca = models.CharField(max_length=20, null=True)
+    cor = models.CharField(max_length=20,blank=True, null=True)
+    genero = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.deveui
