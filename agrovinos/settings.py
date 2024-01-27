7553675be963b3ebc788a677b006b066f9ee4dfe
Welton Sousa <weltonsousa@gmail.com>
2024-01-27 15:13:24 -0300

    Finalizando Atividade 4

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+### Docker Config
+DEBUG = int(os.environ.get("DEBUG", default=DEBUG))
+SECRET_KEY = os.environ.get("SECRET_KEY", default=SECRET_KEY)
+if os.environ.get("DJANGO_ALLOWED_HOSTS"):
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
