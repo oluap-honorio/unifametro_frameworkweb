9b8d5e0515ef218a66e0614a7ca3dd5a65776bd7
miguelferreira <miguel.ferreira@aluno.unifametro.edu.br>
2024-01-27 15:41:31 -0300

    Finalizando Atividade 3

--- agrovinos/urls.py
+++ agrovinos/urls.py
@@ -14,9 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from ovino import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ovino/', include('ovino.urls')),
+    path('', views.index),
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
