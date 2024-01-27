555b212cbb7e8da907d95ca1dd1c1b11d24d9260
LAPTOP-G7JHDVMT\Mattheus Peixoto <mattheuspeixoto>
2024-01-27 14:35:41 -0300

    Finalizando Atividade 4

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -28,6 +28,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+### Docker Config
+DEBUG = int(os.environ.get("DEBUG", default=DEBUG))
+SECRET_KEY = os.environ.get("SECRET_KEY", default=SECRET_KEY)
+if os.environ.get("DJANGO_ALLOWED_HOSTS"):
+    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
 
 # Application definition
 
@@ -38,9 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ovino',
     'sekizai' ,
-    'widget_tweaks' 
+    'widget_tweaks' ,
+    'ovino',
+   
 ]
 
 MIDDLEWARE = [
