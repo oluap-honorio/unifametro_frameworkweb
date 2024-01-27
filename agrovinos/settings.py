eaebd83e5631ec8476ba87ddca16f734df4cbee8
TARGINO0110 <lucianoptargino1@outlook.com>
2024-01-27 14:29:35 -0300

    Finalizando Atividade 3

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ovino.apps.OvinoConfig',
+    'sekizai',
+    'widget_tweaks'
+
 ]
 
 MIDDLEWARE = [
@@ -51,10 +55,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'agrovinos.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
