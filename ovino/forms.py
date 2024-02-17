352a533fb9440331e55fb7711071d3b650edfa81
joelsantos90 <150378874+joelsantos90@users.noreply.github.com>
2024-02-17 19:38:57 +0000

    Finalizando atividade 6

--- ovino/forms.py
+++ ovino/forms.py
@@ -1,17 +1,9 @@
-from django import forms
-from .models import Ovino
-
-
-class OvinoForm(forms.ModelForm):
-
-	class Meta:
-		model = Ovino
-
-		fields = [
-			"identificador",
-            "peso",
-            "nascimento",
-            "raca",
-            "cor",
-            "genero",
-		]
+fields = [
+    "identificador",
+    "peso",
+    "nascimento",
+    "raca",
+    "cor",
+    "genero",
+    "tag",
+]
