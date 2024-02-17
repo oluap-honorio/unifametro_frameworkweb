13d249f0a6e6e20d676e928aae1328c9e53c0187
Tiago Rodrigues <tgrodrigues1987@gmail.com>
2024-02-17 14:42:48 -0300

    Finalizado Atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -129,6 +129,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'agrovinos/static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
