8ba98339bb3fdc5869c0a259d0ec01a6ab98fa8a
kewton-carmo <kewton.carmo@hp.com>
2024-02-17 15:00:51 -0300

    topic/Ativadade-05-KewtonVenancio

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'agrovinos/static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
