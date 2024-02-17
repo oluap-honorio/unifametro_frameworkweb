dbe9f2aa1154134f46c3501702530e9fa57b6a1b
joelsantos90 <150378874+joelsantos90@users.noreply.github.com>
2024-02-17 17:22:30 +0000

    alteracao

--- ovino/models.py
+++ ovino/models.py
@@ -14,3 +14,10 @@ class Ovino(models.Model):
 
     def __str__(self):
         return self.deveui
+from django.db import models
+from tag.models import Tag
+
+
+class Ovino(models.Model):
+    
+    tag = models.OneToOneField(Tag, on_delete=models.SET_NULL, null=True)
