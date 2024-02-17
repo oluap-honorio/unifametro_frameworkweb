395e2bca4a51195b8c191f0e0bfd09006f318d6f
felipe <felipe.patricio@rodobank.com.br>
2024-02-17 16:04:52 -0300

    Finalizando Atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'agrovinos/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -134,3 +138,4 @@ DEBUG = int(os.environ.get("DEBUG", default=DEBUG))
 SECRET_KEY = os.environ.get("SECRET_KEY", default=SECRET_KEY)
 if os.environ.get("DJANGO_ALLOWED_HOSTS"):
     ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
