63659ad7c59d8ef8809ab31deca79355d858993a
Tiago Rodrigues <tgrodrigues1987@gmail.com>
2024-02-17 13:56:45 -0300

    Finalizando Atividade 3-atualizado

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -70,13 +71,6 @@ TEMPLATES = [
     },
 ]
 
-import os
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-# Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/ovino/'
 LOGOUT_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'agrovinos.wsgi.application'
