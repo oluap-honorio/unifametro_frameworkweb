05025b495ab5b3955408b81fc5631a9c8a426916
Lab 03 <lab03@ACADEMICO.LOCAL>
2024-02-17 14:56:34 -0300

    Finalinzando atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -138,6 +138,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'agrovinos/static'),
 )
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
