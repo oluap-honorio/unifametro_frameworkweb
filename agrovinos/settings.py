d41a22ee6e14f01aafc70b5e788c4bb9feef6d41
Felipe Souza <48894718+felipesoart@users.noreply.github.com>
2024-02-17 17:40:09 +0000

    atividade 5

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -19,15 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1pm*crjz8*(dzocwtdd+e76ushzy(uku2pm12x#+&m*%^)d6ar'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
-DEBUG = int(os.environ.get("DEBUG", default=DEBUG))
-SECRET_KEY = os.environ.get("SECRET_KEY", default=SECRET_KEY)
+DEBUG = int(os.environ.get("DEBUG", default=1))
+SECRET_KEY = os.environ.get("SECRET_KEY", default='django-insecure-1pm*crjz8*(dzocwtdd+e76ushzy(uku2pm12x#+&m*%^)d6ar')
 if os.environ.get("DJANGO_ALLOWED_HOSTS"):
     ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
