d9955f6832516ca130fae2e51f0f9a8ad634ce5a
Samuel Porto <samuelportodossantos@gmail.com>
2024-02-21 23:37:46 -0300

    Finalizada atividade 6

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,13 @@ RUN pip install --upgrade pip
 COPY ./requirements.txt . 
 RUN pip install -r requirements.txt 
 
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# install psycopg2 dependencies
+RUN apk update \
+ && apk add postgresql-dev gcc python3-dev musl-dev
+
 # copy project
 COPY . .
---
+++ tag/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .serializers import TagSerializer
+from rest_framework import viewsets, permissions
+from .models import Tag
+
+
+class TagViewSet(viewsets.ModelViewSet):
+  queryset = Tag.objects.all()
+  serializer_class = TagSerializer
+  permission_classes = [permissions.IsAuthenticated]
