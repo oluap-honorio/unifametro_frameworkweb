b24a58781ccf5fc48b4c07a38ba825ae58073b0b
Cintia Amorim <cintia.amorim@outlook.com>
2024-02-21 21:17:39 -0300

    Finalizando Atividade 6

--- agrovinos/settings.py
+++ agrovinos/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "ovino",
+    "tag.apps.TagConfig",
 ]
 
 MIDDLEWARE = [
@@ -87,8 +88,12 @@ WSGI_APPLICATION = "agrovinos.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, 'db.sqlite3')),
+        "USER": os.environ.get("SQL_USER", "admin"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "1234"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
@@ -115,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
 TIME_ZONE = "UTC"
 
--- ovino/models.py
+++ ovino/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from tag.models import Tag
 
 class Ovino(models.Model):
     
+    tag = models.OneToOneField(Tag, on_delete=models.SET_NULL, null=True)
     identificador = models.CharField(max_length=16, unique=True)
     peso = models.FloatField()
     nascimento = models.DateTimeField()
@@ -10,6 +12,6 @@ class Ovino(models.Model):
     genero = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return self.deveui
